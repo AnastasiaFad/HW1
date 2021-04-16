60ace0caba79e900f58ef25e224323025be5b96e
Anastasia_Fadeeva <anastasiia.fadieieva@nure.ua>
2021-04-17 00:08:55 +0300

    initial commit

--- functions.js
+++ functions.js
@@ -2,21 +2,39 @@ module.exports = { dayOfWeek, sumAB, wordNum}
 
 // Task 1
 //'Получить строковое название дня недели по номеру дня. '
-var dayOfWeek = function (num) {
-    switch (num) {
-        case 1: return('Monday'); 
-        case 2: return('Tuesday'); 
-        case 3: return('Wednesday'); 
-        case 4: return('Thursday'); 
-        case 5: return('Friday'); 
-        case 6: return('Saturday'); 
-        case 7: return('Sunday'); 
-    }
-    if (num > 7) {
-        return('Такого дня не существует...');
-    }
-}
+// var dayOfWeek = function (num) {
+//     switch (num) {
+//         case 1: return('Monday'); 
+//         case 2: return('Tuesday'); 
+//         case 3: return('Wednesday'); 
+//         case 4: return('Thursday'); 
+//         case 5: return('Friday'); 
+//         case 6: return('Saturday'); 
+//         case 7: return('Sunday'); 
+//     }
+//     if (num > 7) {
+//         return('Такого дня не существует...');
+//     }
+// }
 // Day_of_Week(9);
+function dayOfWeek(number) {
+    if (typeof number !== "number") {
+        return false;
+    }
+    if (number < 1 || number > 7) {
+        return "wrong input, in week only 7 days";
+    }
+    var week = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    return week[number - 1];
+    }
 
 // Task 2 'Найти расстояние между двумя точками в двухмерном декартовом пространстве.'
 // AC(x1,x2)  BC(y1,y2)
@@ -25,9 +43,9 @@ function sumAB(x1, x2, y1, y2) {
     AC = (x2 - x1);
     BC = (y2 - y1);
     AB = Math.sqrt((AC * AC) + (BC * BC));
-    return("Расстояние между двумя точками: ",AB);
+    return(AB);
 }
-//Sum_AB(-1, 3, 2, 9)
+//sumAB(-1, 3, 2, 9)
 
 // TASK 3 'Вводим число(0-999), получаем строку с прописью числа.'
 function wordNum(num) {
--- functions.test.js
+++ functions.test.js
@@ -1,21 +1,54 @@
 var { dayOfWeek, sumAB, wordNum } = require('./functions.js');
-describe('dayOfWeek', function () {
-    it('dayOfWeek is defined', function () {
-        expect(dayOfWeek).toBeDefined();
-    })
-    // test.each([
-    //     [1, 'Monday'],
-    //     [2, 'Tuesday'],
-    //     [3, 'Wednesday'],
-    //     [4, 'Thursday'],
-    //     [5, 'Friday'],
-    //     [6, 'Saturday'],
-    //     [7, 'Sunday'],
-    //     [9, 'Такого дня не существует...']
-    // ])('testing %i with expected result %s', (arg, result) => {
-    //     expect(rating(arg)).toBe(result);
-    // });
-    it('1 is "Monday"', function () {
-        expect(rating(1)).toBe('Monday');
-    })
-})
+// describe('dayOfWeek', function () {
+//     it(" dayOfWeek should be defined ", function () {
+//     expect(dayOfWeek).toBeDefined();
+//     });
+//     it(" dayOfWeek should be function", function () {
+//         expect(typeof dayOfWeek).toBe("function");
+//     });
+//     it(" dayOfWeek should be without arguments", function () {
+//         expect(getStringDayNumber()).toBe(false);
+//     });
+//     it("should be  < 1", function () {
+//         expect(dayOfWeek(0)).toBe("wrong input, in week only 7 days");
+//     });
+//     it("should be  > 7", function () {
+//         expect(dayOfWeek(8)).toBe("wrong input, in week only 7 days");
+//     });
+//     it("should be  1", function () {
+//         expect(dayOfWeek(1)).toBe("Sunday");
+//     });
+//     it("should be  2", function () {
+//         expect(dayOfWeek(2)).toBe("Monday");
+//     });
+//     it("should be  3", function () {
+//         expect(dayOfWeek(3)).toBe("Tuesday");
+//     });
+//     it("should be  4", function () {
+//         expect(dayOfWeek(4)).toBe("Wednesday");
+//     });
+//     it("should be  5", function () {
+//         expect(dayOfWeek(5)).toBe("Thursday");
+//     });
+//     it("should be  6", function () {
+//         expect(dayOfWeek(6)).toBe("Friday");
+//     });
+//     it("should be  7", function () {
+//         expect(dayOfWeek(7)).toBe("Saturday");
+//     });
+// })
+
+describe('sumAB', function () {
+    it("sumAB should be defined ", function () {
+        expect(sumAB).toBeDefined();
+    });
+    it("sumAB should be function", function () {
+        expect(typeof sumAB).toBe("function");
+    });
+    it("sumAB расстояние от точек координат", function () {
+        expect(sumAB(-1, 3, 2, 9)).toBe(8.06225774829855);
+    });
+    it("sumAB should be function", function () {
+        expect(sumAB(-1, 3, 2, 9)).not.toBe(8.062257);
+    });
+});
